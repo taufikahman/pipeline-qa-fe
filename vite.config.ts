9b1099234a30ab7d0c4a2a0b2b2473af7905e3b5
Taufik Ahman <76449425+taufikahman@users.noreply.github.com>
2025-12-27 01:23:39 +0700

    test 4

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+// import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   // server: {
   //   host: "::",
   //   port: 8080,
   // },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()],
   base:  "/pipeline-qa/",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+  // resolve: {
+  //   alias: {
+  //     "@": path.resolve(__dirname, "./src"),
+  //   },
+  // },
+});
